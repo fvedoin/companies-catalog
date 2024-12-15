697e259765748d964aaf506c41147152de5155ad
Fernando <fernando.vgarcia@hotmail.com>
2024-12-15 16:09:42 +0000

    feat: creating companies grid component

--- packages/web/src/pages/home/index.tsx
+++ packages/web/src/pages/home/index.tsx
@@ -1,29 +1,10 @@
-import { useFetchCompanies } from "../../hooks/useFetchCompanies";
-import { IndustryCard } from "../../components/industry-card";
-import { useGroupedIndustries } from "../../hooks/useGroupedCompanies";
-import { Message } from "../../components/message";
+import { CompaniesGrid } from "../../components/companies-grid";
 
 export function Home() {
-  const { companies, error, loading } = useFetchCompanies();
-
-  const groupedIndustries = useGroupedIndustries(companies);
-
-  if (loading) {
-    return <Message text="Loading industries..." />;
-  }
-
-  if (error) {
-    return <Message text={error} />;
-  }
-
   return (
     <main className="container p-4">
       <h1 className="text-gray-800 mb-4">Industry Catalog</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {groupedIndustries.map(item => (
-          <IndustryCard key={item.industry.id} groupedIndustry={item} />
-        ))}
-      </div>
+      <CompaniesGrid />
     </main>
   );
 }
