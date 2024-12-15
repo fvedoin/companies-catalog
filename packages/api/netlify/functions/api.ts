42947bfea07a0c22b647f4ec9b8df3426b3b33a8
Fernando <fernando.vgarcia@hotmail.com>
2024-12-15 14:45:32 +0000

    feat: adding allowed origins

--- packages/api/netlify/functions/api.ts
+++ packages/api/netlify/functions/api.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import serverless from "serverless-http";
 import router from "../../src/routes/companies.route";
+import cors, { CorsOptions } from 'cors';
 
 const api = express();
+
+const corsOptions: CorsOptions = {
+  origin: process.env.ALLOWED_ORIGIN || "*",
+};
+
+api.use(cors(corsOptions));
 
 api.use("/api/", router);
 
