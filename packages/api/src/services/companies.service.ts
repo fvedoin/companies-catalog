4cd28ce18d1591166c340cc456bc3b876cfb531e
Fernando <fernando.vgarcia@hotmail.com>
2024-12-15 12:17:39 +0000

    feat: adjusting file loading

--- packages/api/src/services/companies.service.ts
+++ packages/api/src/services/companies.service.ts
@@ -3,12 +3,7 @@ import * as path from 'path';
 
 const companyService = {
   getCompanies() {
-    const dataFilePath = path.join(
-      process.cwd(),
-      'src',
-      'data',
-      'data.json',
-    );
+    const dataFilePath = path.resolve(__dirname, '../../src/data/data.json');
     const rawData = fs.readFileSync(dataFilePath, 'utf8');
     return JSON.parse(rawData);
   }
